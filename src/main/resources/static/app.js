bde9318aa72bb6777d220bc0aa3de65952415de5
padovese <bhspadoveze@gmail.com>
2019-08-16 00:08:18 -0300

    creating the initial get from a crud

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,24 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope){
-$scope.wineTypes = ['Rosso', 'Bianco', 'Rosé'];
+myApp.controller('mainController', function ($scope) {
+	$scope.wineTypes = ['Rosso', 'Bianco', 'Rosé'];
+
+});
+
+myApp.controller('crudController', function ($scope, $http) {
+	$scope.customers = '';
+
+	$scope.getResults = function(){
+
+	$http.get('/api/customers').then(
+		function(result){
+			$scope.customers = result.data._embedded.customers;
+			console.log($scope.customers);
+			console.log(result)
+		}, function(data, status){
+			console.log(data, status);
+		});
+
+	}
 
 });
