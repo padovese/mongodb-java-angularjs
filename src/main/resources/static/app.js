5df96cb3e2ae3ddb5ac7a21a4152415dad6e8f7e
padovese <bhspadoveze@gmail.com>
2019-09-01 22:16:44 -0300

    refactor: clean the code

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,7 +1,6 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+let wineApp = angular.module('wineApp', ['ngRoute']);
 
-myApp.config(function ($routeProvider) {
-
+wineApp.config(function ($routeProvider) {
 	$routeProvider
 		.when("/", {
 			templateUrl: 'crudContent.html',
@@ -14,15 +13,13 @@ myApp.config(function ($routeProvider) {
 		})
 });
 
-myApp.controller('updateController', function ($scope, $routeParams, $http, $window){
+wineApp.controller('updateController', function ($scope, $routeParams, $http, $window){
 	$scope.name = $routeParams.name;
 	$scope.year = Number($routeParams.year);
 	$scope.wineType = $routeParams.wineType;
 	$scope.self = $routeParams.self;
 
-
 	$scope.update = function(self){
-		console.log(self);
 		$http.put(self, { name: $scope.name, year: $scope.year, wineType: $scope.wineType }).then(
 			function (result) {
 				alert('Update sucessful');
@@ -33,42 +30,35 @@ myApp.controller('updateController', function ($scope, $routeParams, $http, $win
 	}
 });
 
-myApp.controller('crudController', function ($scope, $http) {
-	$scope.name = '';
+wineApp.controller('crudController', function ($scope, $http) {
+    $scope.name = '';
 	$scope.year = '';
 	$scope.wineType = 'RED';
-
 
 	$http.get('/api/wines').then(
 		function (result) {
 			$scope.wines = result.data._embedded.wines;
-			console.log(result)
 		}, function (data, status) {
 			console.log(data, status);
 		});
 
-
-	$scope.setWine = function () {
+    $scope.setWine = function () {
 		$http.post('/api/wines', { name: $scope.name, year: $scope.year, wineType: $scope.wineType }).then(
 			function (result) {
 				$scope.name = '';
 				$scope.year = '';
-				//$scope.wineType = '';
 				$scope.wines.push(result.data);
 			}, function (data, status) {
 				console.log(data, status);
 			});
 	}
 
-	$scope.deleteWine = function (endPoint, i) {
+    $scope.deleteWine = function (endPoint, i) {
 		$http.delete(endPoint).then(
 			function (result) {
-				console.log(endPoint);
 				$scope.wines.splice(i, 1);
 			}, function (data, result) {
-				console.log(endPoint);
+				console.log(data, status);
 			});
-
 	}
-
 });
